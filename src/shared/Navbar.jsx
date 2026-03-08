3ef53306e9e76738474b280267e298deddd15722
Theera12 <aarthy.maaya@gmail.com>
2026-03-07 17:25:25 -0800

    hamburger icon added

--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import styles from './Navbar.module.css';
 import { NavLink } from 'react-router';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
 function Navbar() {
+  const [navbarIcon, setNavbarIcon] = useState(false);
+
+  const handleIcon = () => {
+    setNavbarIcon((prev) => !prev);
+  };
   return (
     <div className={styles.navbarContainer}>
       <h1>
         C<span>OO</span>KB<span>OO</span>K
       </h1>
-      <div className={styles.navlinksContainer}>
+      <button onClick={handleIcon} className={styles.hamburger}>
+        <GiHamburgerMenu />
+      </button>
+
+      <div
+        className={`${styles.navlinksContainer} ${
+          navbarIcon ? styles.showMenu : ''
+        }`}
+      >
         <NavLink
           to="/"
           className={({ isActive }) =>
